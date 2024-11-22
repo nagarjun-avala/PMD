dddef0bb131218e82c9a17fdcd6ef214ebd05b95
Nagarjun A <nagarjun.avala.official@gmail.com>
2024-11-22 13:00:10 +0530

    Making Updates

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,57 +1,71 @@
 import { getAuthUserDetails } from '@/lib/queries'
+import { off } from 'process'
 import React from 'react'
 import MenuOptions from './menu-options'
 
 type Props = {
-    id: string
-    type: "agency" | "subaccount"
+  id: string
+  type: 'agency' | 'subaccount'
 }
 
 const Sidebar = async ({ id, type }: Props) => {
-    const user = await getAuthUserDetails()
-    if (!user) return null
-    if (!user.Agency) return null
-    const details =
-        type === "agency"
-            ? user.Agency
-            : user.Agency.SubAccount.find((subaccount) => subaccount.id === id)
+  const user = await getAuthUserDetails()
+  if (!user) return null
 
-    if (!details) return null
-    const whiteLabeledAgency = user.Agency.whiteLabel
+  if (!user.Agency) return
 
-    let sideBarLogo = user.Agency.agencyLogo || "/assets/plura-logo.svg"
+  const details =
+    type === 'agency'
+      ? user?.Agency
+      : user?.Agency.SubAccount.find((subaccount) => subaccount.id === id)
 
-    if (!whiteLabeledAgency) {
-        if (type === "subaccount") {
-            sideBarLogo = user.Agency.SubAccount.find((subaccount) => subaccount.id === id)?.subAccountLogo || user.Agency.agencyLogo
-        }
+  const isWhiteLabeledAgency = user.Agency.whiteLabel
+  if (!details) return
+
+  let sideBarLogo = user.Agency.agencyLogo || '/assets/plura-logo.svg'
+
+  if (!isWhiteLabeledAgency) {
+    if (type === 'subaccount') {
+      sideBarLogo =
+        user?.Agency.SubAccount.find((subaccount) => subaccount.id === id)
+          ?.subAccountLogo || user.Agency.agencyLogo
     }
+  }
 
-    const sidebarOpt = type === "agency" ? user.Agency.SidebarOption || [] : user.Agency.SubAccount.find((subaccount) => subaccount.id === id)?.SidebarOption || []
+  const sidebarOpt =
+    type === 'agency'
+      ? user.Agency.SidebarOption || []
+      : user.Agency.SubAccount.find((subaccount) => subaccount.id === id)
+          ?.SidebarOption || []
 
-    const subAccounts = user.Agency.SubAccount.filter((subaccount) => user.Permissions.find(permition => permition.subAccountId === subaccount.id && permition.access))
-
-    return (
-        <>
-            <MenuOptions
-                defaultOpen={true}
-                details={details}
-                id={id}
-                sidebarLogo={sideBarLogo}
-                sidebarOpt={sidebarOpt}
-                subAccounts={subAccounts}
-                user={user}
-            />
-            <MenuOptions
-                details={details}
-                id={id}
-                sidebarLogo={sideBarLogo}
-                sidebarOpt={sidebarOpt}
-                subAccounts={subAccounts}
-                user={user}
-            />
-        </>
+  const subaccounts = user.Agency.SubAccount.filter((subaccount) =>
+    user.Permissions.find(
+      (permission) =>
+        permission.subAccountId === subaccount.id && permission.access
     )
+  )
+
+  return (
+    <>
+      <MenuOptions
+        defaultOpen={true}
+        details={details}
+        id={id}
+        sidebarLogo={sideBarLogo}
+        sidebarOpt={sidebarOpt}
+        subAccounts={subaccounts}
+        user={user}
+      />
+      <MenuOptions
+        details={details}
+        id={id}
+        sidebarLogo={sideBarLogo}
+        sidebarOpt={sidebarOpt}
+        subAccounts={subaccounts}
+        user={user}
+      />
+    </>
+  )
 }
 
 export default Sidebar
